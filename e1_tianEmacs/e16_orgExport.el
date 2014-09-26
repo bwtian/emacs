a16d3f9fbfb8a0a0e4dfb0b993b2eac09e00fffb
t3500 <bwtian@gmail.com>
2014-09-26 12:50:22 +0900

    + ‘e1_tianEmacs/e11_orgSkeleton.el’ + ‘e1_tianEmacs/e12_orgBabel.el’ + ‘e1_tianEmacs/e13_orgTodo.el’ + ‘e1_tianEmacs/e14_orgTag.el’ + ‘e1_tianEmacs/e15_orgPublish.el’ + ‘e1_tianEmacs/e16_orgExport.el’ + ‘e1_tianEmacs/e19_orgLatex.el’ + ‘e1_tianEmacs/e21_auctex.el’ + ‘e1_tianEmacs/e22_reftex.el’ + ‘e1_tianEmacs/e23_previewTex.el’ ...

---
+++ e1_tianEmacs/e16_orgExport.el
@@ -0,0 +1,10 @@
+
+(setq org-export-babel-evaluate nil)
+(setq org-export-with-timestamps nil)
+(setq org-export-with-todo-keywords nil)
+(setq org-export-latex-hyperref-format "\\ref{%s}")
+(setq org-export-with-sub-superscripts nil) ;; ^:nil
+(setq org-export-allow-BIND 1)
+;; (setq org-export-preserve-breaks t)
+;; (setq org-export-with-section-numbers nil)
+;;(setq org-export-latex-hyperref-format "\\ref{%s}:{%s}")
