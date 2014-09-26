4d517ed8c87779a935baf4b2c4b836e25f1f4c3c
t3500 <bwtian@gmail.com>
2014-09-26 12:57:17 +0900

    + ‘00_setEmacs/elpa/anything-R-0.1.2010/anything-R-autoloads.el’ + ‘00_setEmacs/elpa/anything-R-0.1.2010/anything-R-pkg.el’ + ‘00_setEmacs/elpa/anything-R-0.1.2010/anything-R-pkg.elc’ + ‘00_setEmacs/elpa/anything-R-0.1.2010/anything-R.el’

---
+++ 00_setEmacs/elpa/anything-R-0.1.2010/anything-R-autoloads.el
@@ -0,0 +1,18 @@
+;;; anything-R-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("anything-R-pkg.el" "anything-R.el") (21540
+;;;;;;  58393 268132 402000))
+
+;;;***
+
+(provide 'anything-R-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; anything-R-autoloads.el ends here
---
+++ 00_setEmacs/elpa/anything-R-0.1.2010/anything-R-pkg.el
@@ -0,0 +1,1 @@
+(define-package "anything-R" "0.1.2010" "anything-sources and some utilities for GNU R." (quote nil))
