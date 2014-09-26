5eec15d6c0390c410a50652cb51523bee96dca2d
t3500 <bwtian@gmail.com>
2014-09-26 13:30:26 +0900

    + ‘00_setEmacs/elpa/ac-helm-20140919.917/ac-helm-autoloads.el’ + ‘00_setEmacs/elpa/ac-helm-20140919.917/ac-helm-pkg.el’ + ‘00_setEmacs/elpa/ac-helm-20140919.917/ac-helm-pkg.elc’ + ‘00_setEmacs/elpa/ac-helm-20140919.917/ac-helm.el’ + ‘00_setEmacs/elpa/ac-helm-20140919.917/ac-helm.elc’ + ‘00_setEmacs/elpa/helm-ack-20140507.2127/helm-ack-autoloads.el’ + ‘00_setEmacs/elpa/helm-ack-20140507.2127/helm-ack-pkg.el’ + ‘00_setEmacs/elpa/helm-ack-20140507.2127/helm-ack-pkg.elc’ + ‘00_setEmacs/elpa/helm-ack-20140507.2127/helm-ack.el’ + ‘00_setEmacs/elpa/helm-ack-20140507.2127/helm-ack.elc’ ...

---
+++ 00_setEmacs/elpa/helm-chrome-20140116.252/helm-chrome-autoloads.el
@@ -0,0 +1,29 @@
+;;; helm-chrome-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-chrome-bookmarks) "helm-chrome" "helm-chrome.el"
+;;;;;;  (21540 60377 826063 174000))
+;;; Generated autoloads from helm-chrome.el
+
+(autoload 'helm-chrome-bookmarks "helm-chrome" "\
+Search Chrome Bookmark using `helm'.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-chrome-pkg.el") (21540 60377 886115
+;;;;;;  875000))
+
+;;;***
+
+(provide 'helm-chrome-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-chrome-autoloads.el ends here
