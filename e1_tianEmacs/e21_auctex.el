7271df7a1bf203122b6f07fc2991dba6346de1a9
Bingwei Tian <bwtian@gmail.com>
2014-10-04 19:51:42 +0900

    + ‘e1_tianEmacs/e13_orgTodo.el’ + ‘e1_tianEmacs/e14_orgTag.el’ + ‘e1_tianEmacs/e15_orgPublish.el’ + ‘e1_tianEmacs/e16_orgExport.el’ + ‘e1_tianEmacs/e19_orgLatex.el’ + ‘e1_tianEmacs/e21_auctex.el’ + ‘e1_tianEmacs/e22_reftex.el’ + ‘e1_tianEmacs/e23_previewTex.el’ + ‘e1_tianEmacs/e30_ESS.el’ + ‘e1_tianEmacs/e33_markdown.el’ ...

---
+++ e1_tianEmacs/e21_auctex.el
@@ -0,0 +1,18 @@
+
+;; (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
+  (require 'tex-site)   ; invoke the AUCTeX package (LaTeX support)
+  (if (string-equal system-type "windows-nt")
+      (require 'tex-mik))
+;(setq TeX-file-extensions
+;      '("Snw" "Rnw" "nw" "tex" "sty" "cls" "ltx" "texi" "texinfo"))
+
+(load "auctex-pkg.el" nil t t)
+(load "latex.el" nil t t)
+
+(setq TeX-auto-save t)
+(setq LaTeX-math-mode t)
+(setq TeX-electric-escape t)
+(setq TeX-parse-self t)
+
+(setq-default TeX-master nil)
+;;(setq TeX-PDF-mode t) ;; compile documents to PDF by default
