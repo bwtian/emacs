2415996edf9d75589b1441c9814987a74499145f
Bingwei Tian <bwtian@gmail.com>
2014-10-04 21:24:19 +0900

    - ‘e1_tianEmacs/ac-comphist.dat’ - ‘e1_tianEmacs/auto-save-list/’ / ‘e1_tianEmacs/e21_auctex.el’

--- e1_tianEmacs/e21_auctex.el
+++ e1_tianEmacs/e21_auctex.el
@@ -1,46 +1,58 @@
 
 ;; (setenv "PATH" (concat "/usr/texbin:" (getenv "PATH")))
-  (require 'tex-site)   ; invoke the AUCTeX package (LaTeX support)
-  (if (string-equal system-type "windows-nt")
-      (require 'tex-mik))
-(setq TeX-file-extensions
-      '("Snw" "Rnw" "nw" "tex" "sty" "cls" "ltx" "texi" "texinfo"))
+(require 'tex-site)   ; invoke the AUCTeX package (LaTeX support)
+(if (string-equal system-type "windows-nt")
+    (require 'tex-mik))
 
 (load "auctex.el" nil t t)
+(load "preview.el" nil t t)
+(setq TeX-file-extensions
+      '("Snw" "Rnw" "nw" "tex" "sty" "cls" "ltx" "texi" "texinfo"))
+(add-to-list 'auto-mode-alist '("\\.tex$" . LaTeX-mode))
+;;(load "preview-latex.el" nil t t)
 ;;(load "auctex-pkg.el" nil t t)
 ;;(load "latex.el" nil t t) ;; import for not ask master files
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
+(setq TeX-electric-escape t)
 (setq-default TeX-master nil)
 
-(setq LaTeX-math-mode t)
-(setq TeX-electric-escape t)
-(setq TeX-engine 'pdflatex)
-(setq TeX-PDF-mode t) ;; use pdflatex instead of latex by default
 
-(setq TeX-source-correlate-method 'synctex)
-;; Enable synctex generation. Even though the command shows
-;; as "latex" pdflatex is actually called
-(custom-set-variables '(LaTeX-command "latex -synctex=1") )
+(require 'cdlatex)
+(add-hook 'LaTeX-mode-hook 'turn-on-cdlatex) ; with AUCTeX LaTeX mode 
 
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'linum-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'folding-mode)
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-;; enable auto-fill mode, nice for text
-(add-hook 'LaTeX-mode-hook 'auto-fill-mode)
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
 
-;;(load "preview-latex.el" nil t t)
-(load "preview.el" nil t t)
+;(setq LaTeX-math-mode t)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(setq TeX-engine 'pdflatex)
+;(setq TeX-PDF-mode t) ;; use pdflatex instead of latex by default
+(add-hook 'LaTeX-mode-hook 'TeX-PDF-mode) ;Take this out to compile to DVI, instead.
+
+(setq TeX-source-correlate-method 'synctex)
+;; Enable synctex generation. Even though the command shows
+;; as "latex" pdflatex is actually called
+(custom-set-variables '(LaTeX-command "latex -synctex=1") )
+
+(add-hook 'LaTeX-mode-hook
+               (lambda ()
+                 (setq TeX-auto-untabify t     ; remove all tabs before saving
+                       ;;TeX-engine 'xetex       ; use xelatex default
+                       TeX-show-compilation t) ; display compilation windows
+                 (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
+                 (setq TeX-save-query nil)
+                 (imenu-add-menubar-index)
+                 ;;(setq TeX-open-quote "«~")
+                 ;;(setq TeX-close-quote "~»")
+                 (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
 
 ;;(require 'latex-pretty-symbols)
-
-(add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Use Okular as the pdf viewer. Build okular 
@@ -50,33 +62,6 @@
  '((output-pdf "PDF Viewer")))
 (setq TeX-view-program-list
  '(("PDF Viewer" "okular --unique %o#src:%n%b")))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (setq TeX-view-program-list
-        '(("SumatraPDF" "SumatraPDF.exe %o") ;;Windows
-          ("Gsview" "gsview32.exe %o")
-          ("Okular" "okular --unique %o")
-          ("Evince" "evince %o")    ;; Gnomeers
-          ("Firefox" "firefox %o")))
-  (cond
-   ((eq system-type 'windows-nt)
-    (add-hook 'LaTeX-mode-hook
-              (lambda ()
-                (setq TeX-view-program-selection '((output-pdf "SumatraPDF")
-                                                   (output-dvi "Yap"))))))
-   ((eq system-type 'gnu/linux)
-    (add-hook 'LaTeX-mode-hook
-              (lambda ()
-                (setq TeX-view-program-selection '((output-pdf "Okular")
-                                                   (output-dvi "Okular")))))))
-
-  (setq TeX-output-view-style (quote (
-                                      ("^pdf$" "." "evince %o")
-                                      ("^ps$" "." "gv %o")
-                                      ("^dvi$" "." "xdvi %o")
-                                      )))
-  (setq tex-dvi-view-command "xdvi")
-  (setq tex-dvi-print-command "dvips")
-  (setq tex-alt-dvi-print-command "dvips")
 
 ;; sudo apt-get install whizzytex
 (autoload 'whizzytex-mode "whizzytex" "WhizzyTeX, a minor-mode WYSIWIG environment for LaTeX" t)
