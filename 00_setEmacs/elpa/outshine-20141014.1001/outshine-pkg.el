0976b9c8d3e7bd1acbac13ad271d5fc2d637a6f8
t3500 <bwtian@gmail.com>
2014-10-20 10:02:12 +0900

    + ‘00_setEmacs/elpa/org-eldoc-20141018.1633/org-eldoc-autoloads.el’ + ‘00_setEmacs/elpa/org-eldoc-20141018.1633/org-eldoc-pkg.el’ + ‘00_setEmacs/elpa/org-eldoc-20141018.1633/org-eldoc-pkg.elc’ + ‘00_setEmacs/elpa/org-eldoc-20141018.1633/org-eldoc.el’ + ‘00_setEmacs/elpa/org-eldoc-20141018.1633/org-eldoc.elc’ + ‘00_setEmacs/elpa/outshine-20141014.1001/outshine-autoloads.el’ + ‘00_setEmacs/elpa/outshine-20141014.1001/outshine-pkg.el’ + ‘00_setEmacs/elpa/outshine-20141014.1001/outshine-pkg.elc’ + ‘00_setEmacs/elpa/outshine-20141014.1001/outshine.el’ + ‘00_setEmacs/elpa/ox-reveal-20141016.840/ox-reveal-autoloads.el’ ...

---
+++ 00_setEmacs/elpa/outshine-20141014.1001/outshine-pkg.el
@@ -0,0 +1,1 @@
+(define-package "outshine" "20141014.1001" "outline with outshine outshines outline" (quote nil))
---
+++ 00_setEmacs/elpa/ox-reveal-20141016.840/ox-reveal-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ox-reveal" "20141016.840" "reveal.js Presentation Back-End for Org Export Engine" (quote ((org "8.0"))))
