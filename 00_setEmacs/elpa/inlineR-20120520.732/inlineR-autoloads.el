e07a1d70537291caa300d8b4bb274d4118913762
Bingwei Tian <bwtian@gmail.com>
2014-10-25 20:49:48 +0900

    - ‘00_setEmacs/00_initEmacs/orgEmacs/.#e30_ESS.org’ / ‘00_setEmacs/00_initEmacs/orgEmacs/e30_ESS.org’ + ‘00_setEmacs/elpa/inlineR-20120520.732/inlineR-autoloads.el’ + ‘00_setEmacs/elpa/inlineR-20120520.732/inlineR-pkg.el’ + ‘00_setEmacs/elpa/inlineR-20120520.732/inlineR.el’

---
+++ 00_setEmacs/elpa/inlineR-20120520.732/inlineR-autoloads.el
@@ -0,0 +1,18 @@
+;;; inlineR-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("inlineR-pkg.el" "inlineR.el") (21579
+;;;;;;  36429 810959 70000))
+
+;;;***
+
+(provide 'inlineR-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; inlineR-autoloads.el ends here
---
+++ 00_setEmacs/elpa/inlineR-20120520.732/inlineR-pkg.el
@@ -0,0 +1,1 @@
+(define-package "inlineR" "20120520.732" "insert Tag for inline image of R graphics" (quote nil))
