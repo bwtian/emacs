ae63b6693f5cedab9af620716b28a9206312bff5
Bingwei Tian <bwtian@gmail.com>
2014-10-26 01:09:11 +0900

    / ‘ac-comphist.dat’ / ‘e1_tianEmacs/e06_autocomplete.el’

--- e1_tianEmacs/e06_autocomplete.el
+++ e1_tianEmacs/e06_autocomplete.el
@@ -20,10 +20,10 @@
 (global-auto-composition-mode 1)
 (ac-flyspell-workaround)    ;; conflict with flyspell 
 (setq 
-      ac-auto-start 2 ;; t conflict with ESS, complete form fourth character, t=2 
-      ;;ac-trigger-key "TAB" ;;ac-auto-start nil + ac-trigger-key "TAB"  "<C-tab>"
-      ac-delay 0.2 ;; 0.1 fast for fisrt complete ; tiem setting very import to R   
-      ac-auto-show-menu nil ;; nil or ; tiem setting very import to R
+      ac-auto-start nil ;; t conflict with ESS, complete form fourth character, t=2 
+      ac-trigger-key  "<C-tab>" ;;ac-auto-start nil + ac-trigger-key "TAB"  "<C-tab>"
+      ;;ac-delay 0.2 ;; 0.1 fast for fisrt complete ; tiem setting very import to R   
+      ac-auto-show-menu 0.2 ;; nil or ; tiem setting very import to R
 
       ;;ac-show-menu-immediately-on-auto-complete t
       ;; ac-candidate-limit 25 ;; nil
