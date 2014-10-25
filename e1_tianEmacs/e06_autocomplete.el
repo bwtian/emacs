84357f69b14aa6ac935e7be7aba6dc23da29cff6
Bingwei Tian <bwtian@gmail.com>
2014-10-25 12:48:09 +0900

    - ‘00_setEmacs/00_initEmacs/orgEmacs/.#e06_autocomplete.org’ / ‘00_setEmacs/00_initEmacs/orgEmacs/e06_autocomplete.org’ / ‘ac-comphist.dat’ / ‘e1_tianEmacs/e06_autocomplete.el’

--- e1_tianEmacs/e06_autocomplete.el
+++ e1_tianEmacs/e06_autocomplete.el
@@ -1,24 +1,24 @@
 
-;;(require 'auto-complete-config nil 'noerror)
-  (require 'auto-complete-config)  
-  (require 'auto-complete)
-  (load "auto-complete") 
-  (global-auto-complete-mode t) ;; ac all mode
-  (global-auto-composition-mode 1)
-  (ac-flyspell-workaround)    ;; conflict with flyspell 
-  (setq 
-   ac-delay 0.01 ;; 0.1 fast for fisrt complete 
-   ac-auto-start 3 ;; t conflict with ESS, complete form fourth character, t=2 
-   ac-trigger-key nil ;;ac-auto-start nil + ac-trigger-key "TAB"
-   ac-auto-show-menu 0.05 ;;0.001 ;; nil show menu with 0.05 delay
-   ;;ac-show-menu-immediately-on-auto-complete t
-   ;; ac-candidate-limit 25 ;; nil
-   ac-use-comphist t ;; sort Candidate
-   ac-menu-height 20 ;;12 Max height for complete candidate menu
-   ac-ignore-case 'smart
-   ac-fuzzy-enable t ;; Fuzzy mode
-   ac-dwim nil    ;; t DO What I mean nil pop-ups with docs even if a word is uniquely completed
-)
+(require 'auto-complete-config nil 'noerror)
+(require 'auto-complete-config)  
+(require 'auto-complete)
+(load "auto-complete") 
+(global-auto-complete-mode t) ;; ac all mode
+(global-auto-composition-mode 1)
+(ac-flyspell-workaround)    ;; conflict with flyspell 
+(setq 
+ ac-delay 0.01 ;; 0.1 fast for fisrt complete 
+ ac-auto-start 3 ;; t conflict with ESS, complete form fourth character, t=2 
+ ac-trigger-key nil ;;ac-auto-start nil + ac-trigger-key "TAB"
+ ac-auto-show-menu 0.05 ;;0.001 ;; nil show menu with 0.05 delay
+ ;;ac-show-menu-immediately-on-auto-complete t
+ ;; ac-candidate-limit 25 ;; nil
+ ac-use-comphist t ;; sort Candidate
+ ac-menu-height 20 ;;12 Max height for complete candidate menu
+ ac-ignore-case 'smart
+ ac-fuzzy-enable t ;; Fuzzy mode
+ ac-dwim nil    ;; t DO What I mean nil pop-ups with docs even if a word is uniquely completed
+ )
 
 ;; (add-to-list 'ac-dictionary-directories (expand-file-name
    ;;              "~/.emacs.d/elpa/auto-complete-1.4.20110207/dict"))
@@ -56,10 +56,10 @@
   (setq
    ;; ac-use-quick-help nil                   ; no tool tip
    ac-use-quick-help t ;; use quick help
-   ;;ac-quick-help-prefer-pos-tip t
+   ac-quick-help-prefer-pos-tip t
    ac-quick-help-delay 0.1 ;;
    ac-quick-help-height 25
-   ;; ac-quick-help-scroll-down
+   ac-quick-help-scroll-down
    )
   ;; ac-Popup background colors
   (set-face-attribute 'ac-candidate-face nil   :background "#00222c" :foreground "light gray") ;; pop menu
