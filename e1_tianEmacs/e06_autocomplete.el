a57939ace9d0f660f8968e4d66601baf8366c0fd
Bingwei Tian <bwtian@gmail.com>
2014-10-25 13:07:52 +0900

    - ‘00_setEmacs/00_initEmacs/orgEmacs/.#e06_autocomplete.org’ / ‘00_setEmacs/00_initEmacs/orgEmacs/e06_autocomplete.org’ / ‘ac-comphist.dat’ / ‘e1_tianEmacs/e06_autocomplete.el’

--- e1_tianEmacs/e06_autocomplete.el
+++ e1_tianEmacs/e06_autocomplete.el
@@ -189,3 +189,16 @@
 (require 'company-auctex)
 (require 'auto-complete-auctex)
 (company-auctex-init)
+
+(global-set-key "\C-o" 'hippie-expand)
+(setq hippie-expand-try-functions-list
+      '(yas/hippie-try-expand
+        try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
