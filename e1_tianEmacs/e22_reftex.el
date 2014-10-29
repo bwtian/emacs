259c582cc0225510dd5142fd9377188fd7b8f0b8
t3500 <bwtian@gmail.com>
2014-10-29 17:41:07 +0900

    - ‘e1_tianEmacs/.#e22_reftex.el’ / ‘e1_tianEmacs/e22_reftex.el’ + ‘e1_tianEmacs/e30_ESS.el’ + ‘e1_tianEmacs/e33_markdown.el’ + ‘e1_tianEmacs/e34_orgKnitr.el’ + ‘e1_tianEmacs/e35_polymode.el’

--- e1_tianEmacs/e22_reftex.el
+++ e1_tianEmacs/e22_reftex.el
@@ -34,3 +34,42 @@
 
 (add-hook 'latex-mode-hook 'turn-on-reftex) ; with Emacs latex mode
 (add-hook 'Tex-mode-hook 'turn-on-reftex) ; with Emacs latex mode
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex) ; with AUCTeX LaTeX mode
+(setq reftex-plug-into-AUCTeX t)
+(setq reftex-plug-into-auctex t)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex) ; with Rnw mode
+
+(add-hook 'yatex-mode-hook 'turn-on-reftex) ; with YaTeX mode
+
+(setq org-latex-pdf-process
+          '("pdflatex -interaction nonstopmode -output-directory %o %f"
+            "bibtex %b"
+            "pdflatex -interaction nonstopmode -output-directory %o %f"
+            "pdflatex -interaction nonstopmode -output-directory %o %f"))
+  ;; (setq org-latex-pdf-process
+  ;;         '("pdflatex -interaction nonstopmode -output-directory %o %f"
+  ;;           "biber %b"
+  ;;           "pdflatex -interaction nonstopmode -output-directory %o %f"
+  ;;           "pdflatex -interaction nonstopmode -output-directory %o %f"))
+;;;; biber
+;; (setq org-latex-to-pdf-process 
+;;    '("pdflatex %f" "biber %b" "pdflatex %f" "pdflatex %f"))
+
+(defun my-bibliography-selector-hook (backend)
+    (case backend
+      (latex
+       (when (save-excursion
+               (re-search-forward "^[ \t]*\\bibliography\\(?:style\\)?{" nil t))
+         (while (re-search-forward "^[ \t]*#+BIBLIOGRAPHY:.*$" nil t)
+           (when (eq (org-element-type (save-match-data (org-element-at-point)))
+                     'keyword)
+             (replace-match "")))))
+      (html
+       (when (save-excursion
+               (re-search-forward "^[ \t]*#+BIBLIOGRAPHY:.*$" nil t))
+         (while (re-search-forward "^[ \t]*\\bibliography\\(?:style\\)?{.*$" nil t)
+           (replace-match ""))))))
+
+(add-hook 'org-export-before-parsing-hook 'my-bibliography-selector-hook)
