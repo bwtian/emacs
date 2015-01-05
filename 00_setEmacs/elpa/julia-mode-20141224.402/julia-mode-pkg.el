984bd8b72764fae3fa9870dc81b45bcd2613eab6
tian <bwtian@gmail.com>
2015-01-06 08:08:36 +0900

    - ‘00_setEmacs/elpa/async-20141224.2140/async-autoloads.el’ - ‘00_setEmacs/elpa/async-20141224.2140/async-bytecomp.el’ - ‘00_setEmacs/elpa/async-20141224.2140/async-bytecomp.elc’ - ‘00_setEmacs/elpa/async-20141224.2140/async-pkg.el’ - ‘00_setEmacs/elpa/async-20141224.2140/async.elc’ - ‘00_setEmacs/elpa/async-20141224.2140/dired-async.elc’ - ‘00_setEmacs/elpa/async-20141224.2140/smtpmail-async.elc’ + ‘00_setEmacs/elpa/async-20141229.2221/async-autoloads.el’ + ‘00_setEmacs/elpa/async-20141229.2221/async-bytecomp.el’ + ‘00_setEmacs/elpa/async-20141229.2221/async-bytecomp.elc’ ...

--- 00_setEmacs/elpa/julia-mode-20141224.402/julia-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "julia-mode" "20141224.402" "Major mode for editing Julia source code" (quote nil))
--- 00_setEmacs/elpa/yaxception-20140327.409/yaxception-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; yaxception-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("yaxception-pkg.el" "yaxception.el") (21501
-;;;;;;  17404 102463 865000))
-
-;;;***
-
-(provide 'yaxception-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; yaxception-autoloads.el ends here
