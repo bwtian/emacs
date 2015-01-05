4c9f239a8e936b6c4ecef81af66a734b320454d0
tian <bwtian@gmail.com>
2015-01-06 08:14:16 +0900

    + ‘00_setEmacs/elpa/company-c-headers-20140930.1102/company-c-headers-autoloads.el’ + ‘00_setEmacs/elpa/company-c-headers-20140930.1102/company-c-headers-pkg.el’ + ‘00_setEmacs/elpa/company-c-headers-20140930.1102/company-c-headers-pkg.elc’ + ‘00_setEmacs/elpa/company-c-headers-20140930.1102/company-c-headers.el’ + ‘00_setEmacs/elpa/company-c-headers-20140930.1102/company-c-headers.elc’ + ‘00_setEmacs/elpa/company-ess-20141214.1557/company-ess-autoloads.el’ + ‘00_setEmacs/elpa/company-ess-20141214.1557/company-ess-pkg.el’ + ‘00_setEmacs/elpa/company-ess-20141214.1557/company-ess-pkg.elc’ + ‘00_setEmacs/elpa/company-ess-20141214.1557/company-ess.el’ + ‘00_setEmacs/elpa/company-ess-20141214.1557/company-ess.elc’ ...

---
+++ 00_setEmacs/elpa/company-ess-20141214.1557/company-ess-autoloads.el
@@ -0,0 +1,29 @@
+;;; company-ess-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (company-ess-backend) "company-ess" "company-ess.el"
+;;;;;;  (21675 6846 45448 500000))
+;;; Generated autoloads from company-ess.el
+
+(autoload 'company-ess-backend "company-ess" "\
+R/ESS backend for company-mode.
+
+\(fn COMMAND &optional ARG &rest IGNORED)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("company-ess-pkg.el") (21675 6846 49244
+;;;;;;  330000))
+
+;;;***
+
+(provide 'company-ess-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-ess-autoloads.el ends here
---
+++ 00_setEmacs/elpa/company-ess-20141214.1557/company-ess-pkg.el
@@ -0,0 +1,1 @@
+(define-package "company-ess" "20141214.1557" "R/ess Completion Backend for company-mode" (quote ((company "0.8.0") (ess "13.5") (cl-lib "0.5") (emacs "24.1"))))
