8fa2aedba213b37248ce21f5ba12ae25d5f2c510
tian <bwtian@gmail.com>
2015-01-27 09:30:00 +0900

    + ‘00_setEmacs/elpa/helm-ls-git-20150119.719/helm-ls-git-autoloads.el’ + ‘00_setEmacs/elpa/helm-ls-git-20150119.719/helm-ls-git-pkg.el’ + ‘00_setEmacs/elpa/helm-ls-git-20150119.719/helm-ls-git-pkg.elc’ + ‘00_setEmacs/elpa/helm-ls-git-20150119.719/helm-ls-git.el’ + ‘00_setEmacs/elpa/helm-ls-git-20150119.719/helm-ls-git.elc’ + ‘00_setEmacs/elpa/latex-extra-20150124.640/latex-extra-autoloads.el’ + ‘00_setEmacs/elpa/latex-extra-20150124.640/latex-extra-pkg.el’ + ‘00_setEmacs/elpa/latex-extra-20150124.640/latex-extra-pkg.elc’ + ‘00_setEmacs/elpa/latex-extra-20150124.640/latex-extra.el’ + ‘00_setEmacs/elpa/latex-extra-20150124.640/latex-extra.elc’ ...

---
+++ 00_setEmacs/elpa/helm-ls-git-20150119.719/helm-ls-git-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-ls-git" "20150119.719" "list git files." (quote ((helm "1.5"))))
---
+++ 00_setEmacs/elpa/latex-extra-20150124.640/latex-extra-pkg.el
@@ -0,0 +1,1 @@
+(define-package "latex-extra" "20150124.640" "Adds several useful functionalities to LaTeX-mode." (quote ((auctex "11.86.1") (cl-lib "0.5"))))
