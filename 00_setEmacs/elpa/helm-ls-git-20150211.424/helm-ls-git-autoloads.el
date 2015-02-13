41a25cfbe0ed5a47ca554ae89b9b90a6935c141d
tian <bwtian@gmail.com>
2015-02-13 14:25:20 +0900

    + ‘00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git-autoloads.el’ + ‘00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git-pkg.el’ + ‘00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git-pkg.elc’ + ‘00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git.el’ + ‘00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git.elc’ + ‘00_setEmacs/elpa/org-20150209/org-timer.elc’ + ‘00_setEmacs/elpa/org-20150209/org-w3m.elc’ + ‘00_setEmacs/elpa/org-20150209/org.elc’ + ‘00_setEmacs/elpa/org-20150209/ox-ascii.elc’ + ‘00_setEmacs/elpa/org-20150209/ox-beamer.elc’ ...

---
+++ 00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git-autoloads.el
@@ -0,0 +1,33 @@
+;;; helm-ls-git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (helm-ls-git-ls helm-ls-git-status-source helm-ls-git-source)
+;;;;;;  "helm-ls-git" "helm-ls-git.el" (21725 35519 49842 505000))
+;;; Generated autoloads from helm-ls-git.el
+
+(eieio-defclass-autoload 'helm-ls-git-source '(helm-source-in-buffer) "helm-ls-git" nil)
+
+(eieio-defclass-autoload 'helm-ls-git-status-source '(helm-source-in-buffer) "helm-ls-git" nil)
+
+(autoload 'helm-ls-git-ls "helm-ls-git" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("helm-ls-git-pkg.el") (21725 35519 305062
+;;;;;;  506000))
+
+;;;***
+
+(provide 'helm-ls-git-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; helm-ls-git-autoloads.el ends here
---
+++ 00_setEmacs/elpa/helm-ls-git-20150211.424/helm-ls-git-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-ls-git" "20150211.424" "list git files." (quote ((helm "1.5"))))
