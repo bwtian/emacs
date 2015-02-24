509329ca627d0b24d2fcb2f5f799888ffda3bc7c
tian <bwtian@gmail.com>
2015-02-24 14:33:16 +0900

    - ‘00_setEmacs/elpa/auto-complete-20150201.150/auto-complete-autoloads.el’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/auto-complete-config.el’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/auto-complete-config.elc’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/auto-complete-pkg.el’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/auto-complete.el’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/auto-complete.elc’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/dict/ada-mode’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/dict/c++-mode’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/dict/c-mode’ - ‘00_setEmacs/elpa/auto-complete-20150201.150/dict/caml-mode’ ...

--- 00_setEmacs/elpa/helm-bibtex-20150211.1432/helm-bibtex-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; helm-bibtex-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (helm-bibtex) "helm-bibtex" "helm-bibtex.el" (21725
-;;;;;;  35523 165842 469000))
-;;; Generated autoloads from helm-bibtex.el
-
-(autoload 'helm-bibtex "helm-bibtex" "\
-Search BibTeX entries.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("helm-bibtex-pkg.el") (21725 35523 244178
-;;;;;;  712000))
-
-;;;***
-
-(provide 'helm-bibtex-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; helm-bibtex-autoloads.el ends here
--- 00_setEmacs/elpa/pabbrev-20141021.1434/pabbrev-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pabbrev" "20141021.1434" "Predictive abbreviation expansion" (quote nil))
