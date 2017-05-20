f3964d62ee1c645d826bda837c2a6c1a0757b360
tian <bwtian@gmail.com>
2017-05-20 19:02:22 +0800

    / ‘00_setEmacs/share/ac-comphist.dat’ - ‘e1_tianEmacs/e12_orgBabel.el’ - ‘e1_tianEmacs/e12_orgBabel.org’ + ‘e1_tianEmacs/e13_orgTodo.el’ + ‘e1_tianEmacs/e14_orgTag.el’ + ‘e1_tianEmacs/e15_orgPublish.el’ + ‘e1_tianEmacs/e16_orgExport.el’ + ‘e1_tianEmacs/e17_orgInlineImage.el’ + ‘e1_tianEmacs/e19_orgTex.el’ + ‘e1_tianEmacs/e20_tex.el’

---
+++ e1_tianEmacs/e16_orgExport.el
@@ -0,0 +1,12 @@
+(setq org-export-babel-evaluate nil)
+(setq org-export-with-timestamps nil)
+(setq org-export-with-todo-keywords nil)
+(setq org-export-latex-hyperref-format "\\ref{%s}")
+(setq org-export-with-sub-superscripts nil) ;; ^:nil
+(setq org-export-allow-BIND 1)
+(setq org-babel-latex-htlatex "htlatex")
+  (defmacro by-backend (&rest body)
+    `(case (if (boundp 'backend) (org-export-backend-name backend) nil) ,@body))
+;; (setq org-export-preserve-breaks t)
+;; (setq org-export-with-section-numbers nil)
+;;(setq org-export-latex-hyperref-format "\\ref{%s}:{%s}")
